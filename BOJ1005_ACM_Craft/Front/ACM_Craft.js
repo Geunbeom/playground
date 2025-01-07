22af48d5acac22f1921950cfdf5c9b4b3171b801
서근범 <seo6893@naver.com>
2025-01-07 21:34:48 +0900

    feat: node 추가

--- BOJ1005_ACM_Craft/Front/ACM_Craft.js
+++ BOJ1005_ACM_Craft/Front/ACM_Craft.js
@@ -1,1 +1,16 @@
 document.querySelector(".title").innerText = "ACM Craft 실험실";
+
+const f = document.createElement("div");
+f.style.cssText = "display: flex; flex-wrap:wrap;";
+document.querySelector(".frame").append(f);
+
+const el = new Array(20);
+el.fill(0);
+
+el.forEach(e => {
+    e = document.createElement("div");
+    e.classList = "node"
+    document.querySelector(".frame div").append(e);
+});
+
+// document.querySelector(".frame").innerHTML ="<div>123</div>"
